dff10492525c05f28ab901f3162a3e39418d4dbe
Vincenzo <ilcontevince@gmail.com>
2019-11-19 20:33:59 +0000

    Updates

---
+++ precache-manifest.cd7d378fbcf9c3e46eaf0570b92b3c7a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f0e7899d87a238b575d9fa03d34053d",
+    "url": "/YouTube-API-app/index.html"
+  },
+  {
+    "revision": "d24710ac07dd1af0f7cc",
+    "url": "/YouTube-API-app/static/css/main.12338f81.chunk.css"
+  },
+  {
+    "revision": "e58423b59b49425844d4",
+    "url": "/YouTube-API-app/static/js/2.71ea4311.chunk.js"
+  },
+  {
+    "revision": "d24710ac07dd1af0f7cc",
+    "url": "/YouTube-API-app/static/js/main.188cdb1d.chunk.js"
+  },
+  {
+    "revision": "d3d93df744043b3ba268",
+    "url": "/YouTube-API-app/static/js/runtime-main.56c6678d.js"
+  }
+]);
